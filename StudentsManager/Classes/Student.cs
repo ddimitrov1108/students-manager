d31933185cf3e5678bbd4951e7ccf9b60a75f73f
Daniel D <79416039+ddimitrov1108@users.noreply.github.com>
2022-04-14 14:08:23 +0300

    .

---
+++ StudentsManager/Classes/Student.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+
+namespace StudentsManager.Classes
+{
+    public interface IStudentDatabaseQueries
+    {
+        public bool create(MySqlConnection connection);
+        public void read(MySqlConnection connection);
+        public bool update(MySqlConnection connection);
+        public void delete(MySqlConnection connection);
+
+    }
+    public class Student : IStudentDatabaseQueries
+    {
+        private string facultyNumber;
+        private string firstName, lastName, phoneNumber;
+        private string degreeType, specialtyName, formType;
+        private double gpa;
+        private int year;
+        private bool isEducationPaused;
+
+        public Student()
+        {
+            this.facultyNumber = "";
+            this.firstName = "";
+            this.lastName = "";
+            this.phoneNumber = "";
+            this.degreeType = "";
+            this.specialtyName = "";
+            this.formType = "";
+            this.gpa = 0.00;
+            this.year = 0;
+            this.isEducationPaused = true;
+        }
+
+        public Student(Student studentObject)
+        {
+            this.facultyNumber = studentObject.facultyNumber;
+            this.firstName = studentObject.firstName;
+            this.lastName = studentObject.lastName;
+            this.phoneNumber = studentObject.phoneNumber;
+            this.degreeType = studentObject.degreeType;
+            this.specialtyName = studentObject.specialtyName;
+            this.formType = studentObject.formType;
+            this.gpa = studentObject.gpa;
+            this.year = studentObject.year;
+            this.isEducationPaused = studentObject.isEducationPaused;
+        }
+
+        public Student(string _facNum, string _fn, string _ln, string _phoneNum, string _degreeType, string _specName, string _formType, double _gpa, int _year, bool _isEduPaused)
+        {
+            this.facultyNumber = _facNum;
+            this.firstName = _fn;
+            this.lastName = _ln;
+            this.phoneNumber = _phoneNum;
+            this.degreeType = _degreeType;
+            this.specialtyName = _specName;
+            this.formType = _formType;
+            this.gpa = _gpa;
+            this.year = _year;
+            this.isEducationPaused = _isEduPaused;
+        }
+
+        public string FullName { get => $"{ this.firstName } { this.lastName }"; }
+        public string FacultyNumber { get; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string DegreeType { get; set; }
+        public string SpecialtyName { get; set; }
+        public string FormType { get; set; }
+        public double Gpa { get; set; }
+        public int Year { get; set; }
+        public bool IsEducationPaused { get; set; }
+
+        public override string ToString()
+        {
+            return "{" +
+                $"facultyNumber: { this.facultyNumber }, " +
+                $"fullName: { this.FullName }, " +
+                $"phoneNumber: { this.phoneNumber }, " +
+                $"degree: { this.degreeType }, " +
+                $"specialty: { this.specialtyName }, " +
+                $"form: { this.formType }, " +
+                $"gpa: { this.gpa }, " +
+                $"year: { this.year }, " +
+                $"eduPaused: { this.isEducationPaused.ToString() } " + "}";
+        }
+        public int CompareTo(Student? studentObject)
+        {
+            if (studentObject == null)
+                return 1;
+            else
+                return this.facultyNumber.CompareTo(studentObject.facultyNumber);
+        }
+
+        public override bool Equals(Object? obj)
+        {
+            if (obj == null || !this.GetType().Equals(obj.GetType()))
+                return false;
+
+            Student stud = (Student)obj;
+            return this.facultyNumber.CompareTo(stud.facultyNumber) == 0;
+        }
+
+        public bool create(MySqlConnection connection)
+        {
+            using (MySqlCommand command = connection.CreateCommand())
+            {
+                int degreeId = 0;
+                int specialtyId = 0;
+                int formId = 0;
+
+                command.CommandText = "SELECT id FROM edu_degree_types WHERE degree = ?d";
+                command.Parameters.Add("?d", this.degreeType);
+                MySqlDataReader reader;
+
+                command.CommandText = "INSERT INTO students VALUES(null, ?facNum, ?fn, ?ln, ?phn, ?dt, ?sn, ?ft, ?year, ?gpa, ?edup, default, default)";
+                command.Parameters.AddRange(new MySqlParameter[] {
+                    new MySqlParameter("?facNum", this.facultyNumber),
+                    new MySqlParameter("?fn", this.firstName),
+                    new MySqlParameter("?ln", this.lastName),
+                    new MySqlParameter("?phn", this.phoneNumber),
+                    new MySqlParameter("?dt", this.degreeType),
+                    new MySqlParameter("?sn", this.specialtyName),
+                    new MySqlParameter("?ft", this.formType),
+                    new MySqlParameter("?year", this.year),
+                    new MySqlParameter("?gpa", this.gpa),
+                    new MySqlParameter("?edup", this.isEducationPaused)
+                });
+                return command.ExecuteNonQuery() == 1;
+            }
+        }
+
+        public void read(MySqlConnection connection)
+        {
+
+        }
+
+        public bool update(MySqlConnection connection)
+        {
+            return true;
+        }
+
+        public void delete(MySqlConnection connection)
+        {
+
+        }
+
+    }
+}
---
+++ students_manager.sql
@@ -0,0 +1,230 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Хост: 127.0.0.1
+-- Време на генериране: 15 март 2022 в 10:12
+-- Версия на сървъра: 10.4.21-MariaDB
+-- Версия на PHP: 8.0.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- База данни: `students_manager`
+--
+
+use students_manager;
+
+-- --------------------------------------------------------
+
+--
+-- Структура на таблица `edu_degree_types`
+--
+
+CREATE TABLE `edu_degree_types` (
+  `id` int(11) NOT NULL,
+  `degree` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Схема на данните от таблица `edu_degree_types`
+--
+
+INSERT INTO `edu_degree_types` (`id`, `degree`) VALUES
+(1, 'Bachelor'),
+(2, 'Masters'),
+(3, 'PHD');
+
+-- --------------------------------------------------------
+
+--
+-- Структура на таблица `edu_forms`
+--
+
+CREATE TABLE `edu_forms` (
+  `id` int(11) NOT NULL,
+  `form` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Схема на данните от таблица `edu_forms`
+--
+
+INSERT INTO `edu_forms` (`id`, `form`) VALUES
+(1, 'full-time'),
+(2, 'part-time'),
+(3, 'abroad');
+
+-- --------------------------------------------------------
+
+--
+-- Структура на таблица `edu_specialties`
+--
+
+CREATE TABLE `edu_specialties` (
+  `id` int(11) NOT NULL,
+  `specialty` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Схема на данните от таблица `edu_specialties`
+--
+
+INSERT INTO `edu_specialties` (`id`, `specialty`) VALUES
+(1, 'Computer Science'),
+(2, 'Software Engineering'),
+(3, 'Data Analytics'),
+(4, 'Computer Information Systems'),
+(5, 'Information Technology'),
+(6, 'Management Information Systems');
+
+-- --------------------------------------------------------
+
+--
+-- Структура на таблица `students`
+--
+
+CREATE TABLE `students` (
+  `id` int(11) NOT NULL,
+  `facultyNumber` int(11) NOT NULL,
+  `firstName` varchar(20) NOT NULL,
+  `lastName` varchar(20) NOT NULL,
+  `phoneNumber` varchar(20) NOT NULL,
+  `degreeId` int(11) NOT NULL,
+  `specialtyId` int(11) NOT NULL,
+  `formId` int(11) NOT NULL,
+  `eduYear` int(11) NOT NULL,
+  `eduGpa` double NOT NULL,
+  `eduPaused` tinyint(1) NOT NULL,
+  `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Схема на данните от таблица `students`
+--
+
+INSERT INTO `students` (`id`, `facultyNumber`, `firstName`, `lastName`, `phoneNumber`, `degreeId`, `specialtyId`, `formId`, `eduYear`, `eduGpa`, `eduPaused`, `updated_at`, `created_at`) VALUES
+(1, 19623345, 'Daniel', 'Dimitrov', '+395887575828', 1, 1, 2, 3, 5.14, 0, '2022-03-14 23:38:09', '2022-03-14 21:38:09'),
+(2, 19965197, 'Stoqno', 'Stoqnov', '+395880000000', 1, 2, 1, 4, 5.14, 1, '2022-03-14 23:38:09', '2022-03-14 21:38:09');
+
+-- --------------------------------------------------------
+
+--
+-- Структура на таблица `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `firstName` varchar(20) NOT NULL,
+  `lastName` varchar(20) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `isAdmin` tinyint(1) NOT NULL,
+  `updated_at` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Схема на данните от таблица `users`
+--
+
+INSERT INTO `users` (`id`, `firstName`, `lastName`, `email`, `password`, `isAdmin`, `updated_at`, `created_at`) VALUES
+(1, 'Daniel', 'Dimitrov', 'ddimitrov1108@gmail.com', '$2b$10$ycwTa4c7sGW0Aa3gCroFkuuVlloYD2eoaEUoum/9Y1T1m2JS5Anhu', 1, '2022-03-12 18:14:18', '2022-03-12 16:14:18'),
+(2, 'Stoqn', 'Stoqnov', 'susu2606@gmail.com', '$2b$10$iXyLCjAnJxI6QE8tBjDcjeKGeKK9xUAlkmyhHgyyUDJpfdas8x6SG', 0, '2022-03-12 18:15:12', '2022-03-12 16:15:12');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Индекси за таблица `edu_degree_types`
+--
+ALTER TABLE `edu_degree_types`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индекси за таблица `edu_forms`
+--
+ALTER TABLE `edu_forms`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индекси за таблица `edu_specialties`
+--
+ALTER TABLE `edu_specialties`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индекси за таблица `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `degreeId` (`degreeId`),
+  ADD KEY `specialtyId` (`specialtyId`),
+  ADD KEY `formId` (`formId`);
+
+--
+-- Индекси за таблица `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `edu_degree_types`
+--
+ALTER TABLE `edu_degree_types`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `edu_forms`
+--
+ALTER TABLE `edu_forms`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `edu_specialties`
+--
+ALTER TABLE `edu_specialties`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Ограничения за дъмпнати таблици
+--
+
+--
+-- Ограничения за таблица `students`
+--
+ALTER TABLE `students`
+  ADD CONSTRAINT `students_ibfk_1` FOREIGN KEY (`degreeId`) REFERENCES `edu_degree_types` (`id`),
+  ADD CONSTRAINT `students_ibfk_2` FOREIGN KEY (`specialtyId`) REFERENCES `edu_specialties` (`id`),
+  ADD CONSTRAINT `students_ibfk_3` FOREIGN KEY (`formId`) REFERENCES `edu_forms` (`id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
