a0960dabf772beb378943ffe0571e81d61ff0869
Daniel D <79416039+ddimitrov1108@users.noreply.github.com>
2022-04-20 15:12:13 +0300

    .

---
+++ StudentsManager/Components/CustomInput.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsManager.Components
+{
+    public partial class CustomInput : Component
+    {
+        public CustomInput()
+        {
+            InitializeComponent();
+        }
+
+        public CustomInput(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
